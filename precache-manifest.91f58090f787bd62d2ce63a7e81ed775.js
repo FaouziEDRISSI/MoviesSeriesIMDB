dd1a73dd4df5259bbaf04bfbd9a58685b9fd4a00
faouzi edrissi <faouziedrissi@MacBook-Pro-de-faouzi.local>
2020-01-18 18:28:25 +0100

    Updates

---
+++ precache-manifest.91f58090f787bd62d2ce63a7e81ed775.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88036d10a028efcf7340d0d3af8c912",
+    "url": "/MoviesSeriesIMDB/index.html"
+  },
+  {
+    "revision": "7e15e8bdb0ea285cbe77",
+    "url": "/MoviesSeriesIMDB/static/css/main.d14e4011.chunk.css"
+  },
+  {
+    "revision": "d40cf567af589a7ee861",
+    "url": "/MoviesSeriesIMDB/static/js/2.5d5a4904.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/MoviesSeriesIMDB/static/js/2.5d5a4904.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e15e8bdb0ea285cbe77",
+    "url": "/MoviesSeriesIMDB/static/js/main.6baab9d5.chunk.js"
+  },
+  {
+    "revision": "524b353488971d59d8c0",
+    "url": "/MoviesSeriesIMDB/static/js/runtime-main.ea1645de.js"
+  }
+]);
